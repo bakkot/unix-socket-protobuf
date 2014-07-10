cd69a55f820e2a5904dbf1300933a2f473d4c587
Kevin Gibbons <kevin.gibbons@gmail.com>
2014-07-09 17:17:20 -0700

    go server written

--- Go/src/client/main.go
+++ Go/src/client/main.go
@@ -1,47 +1,35 @@
 package main
 
 import (
+	"encoding/binary"
 	"log"
 	"net"
-	//"fmt"
-	//"bytes"
-	"encoding/binary"
 
 	"code.google.com/p/goprotobuf/proto"
 	"example_mess"
 )
 
-var _ = net.Dial
-//var _ = bytes.Buffer
+func passOrDie(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-
 	socket, err := net.Dial("unix", "socket")
-	if err!= nil {
-		log.Fatal("Failed to connect to socket: ", err)
-	}
-	
+	passOrDie(err)
 	defer socket.Close()
 
-	test := &example_mess.Boring {
+	msg := &example_mess.Boring{
 		Cont: proto.String("Greets from Go"),
 	}
-	serialized, err := proto.Marshal(test)
-	if err != nil {
-		log.Fatal("marshaling error: ", err)
-	}
-	
-	
-	//buf := new(bytes.Buffer)
-	println(len(serialized))
-	//err = binary.Write(buf, binary.BigEndian, int32(1337))
-	//fmt.Printf("% d", buf.Bytes())
-	
+
+	serialized, err := proto.Marshal(msg)
+	passOrDie(err)
+
 	// Go is unique in that, thanks to its interfaces, it's practical to encode directly to the socket
 	err = binary.Write(socket, binary.BigEndian, int32(len(serialized)))
-	if err != nil {
-		log.Fatal("failed writing to socket: ", err)
-	}
-	
+	passOrDie(err)
+
 	socket.Write(serialized)
 }
--- Go/src/server/main.go
+++ Go/src/server/main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"encoding/binary"
 	"log"
+	"net"
+	"syscall"
 
 	"code.google.com/p/goprotobuf/proto"
 	"example_mess"
 )
 
-func main() {
-	test := &example_mess.Boring {
-		Cont: proto.String("wew"),
-	}
-	_, err := proto.Marshal(test)
+func passOrDie(err error) {
 	if err != nil {
-		log.Fatal("marshaling error: ", err)
+		log.Fatal(err)
 	}
+}
+
+func handler(connection net.Conn) {
+	var message_length int32
+
+	// encoding/binary is quite clever! Notice that we're reading from the socket and encoding directly into the int32 here. Exactly 4 bytes (the size of message_length) will be read.
+	err := binary.Read(connection, binary.BigEndian, &message_length)
+	passOrDie(err)
+
+	println(message_length)
+
+	msg := new(example_mess.Boring)
+
+	buf := make([]byte, message_length)
+	err = binary.Read(connection, binary.BigEndian, buf)
+	passOrDie(err)
+	err = proto.Unmarshal(buf, msg)
+	passOrDie(err)
+
+	println("Message: ", msg.GetCont())
+
+}
+
+func main() {
+	syscall.Unlink("socket")
+	listener, err := net.Listen("unix", "socket")
+	passOrDie(err)
+
+	for {
+		connection, err := listener.Accept()
+		passOrDie(err)
+
+		go handler(connection)
+	}
+
 }
